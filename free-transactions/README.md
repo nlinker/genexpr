0cf9d3dbc843f9b7746d4d21df568f715f574b94
Nick Linker <nlinker@gmail.com>
2017-01-09 13:32:53 +0600

    making transact polymorphic

---
+++ free-transactions/README.md
@@ -0,0 +1,38 @@
+
+type Action n v i = FreeT (ActionF n v i)
+
+data ActionF n v i a =
+  NewNode v (n -> a) |
+  GetValue n (v -> a) |
+  SetValue n v a |
+  GetRoot (n -> a) |
+  GetTargets n i ([n] -> a) |
+  AddTarget n n a |
+  RemoveTarget n n a |
+  Remove n a |
+  GetStats ((Int, Int, Int) -> a)
+  deriving (Functor)
+
+makeFree ''ActionF
+
+
+-- from https://github.com/dhess/free-experiments/blob/3e878ec743df9045ea9b76c85f8184e38d0807e2/src/RetryTransTH.hs
+data RetryF m next where
+  Output    :: String -> next -> RetryF m next
+  Input     :: Read a => (a -> next) -> RetryF m next
+  WithRetry :: (RetryT m) m a -> (a -> next) -> RetryF m next
+  Retry     :: RetryF m next
+
+-- data (Read a) => RetryF m a next = Output String next
+--                                  | Input (a -> next)
+--                                  | WithRetry ((RetryT m) m a) (a -> next)
+--                                  | Retry
+
+-- type RetryT m = FreeT (RetryF m a)
+type RetryT m = FreeT (RetryF m)
+
+instance Functor (RetryF m) where
+  fmap f (Output s x) = Output s (f x)
+  fmap f (Input g) = Input (f . g)
+  fmap f (WithRetry block g) = WithRetry block (f . g)
+  fmap _ Retry = Retry
--- free-transactions/src/Lib.hs
+++ free-transactions/src/Lib.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs #-}
 
 module Lib where
@@ -15,56 +17,38 @@ import Data.Text              (Text)
 
 type Key = Text
 type Val = Text
-data Ops next = Get Key (Maybe Val -> next)
-                  | Put Key Val next
-                  | Transact (OpsM ()) next
-                    deriving (Functor)
+data OpsF a b next = Get Key (Val -> next)
+                | Put Key Val next
+                | Transact (OpsM a b) next
+                  deriving (Functor)
 
-type OpsM = Free Ops
-
--- from https://github.com/dhess/free-experiments/blob/3e878ec743df9045ea9b76c85f8184e38d0807e2/src/RetryTransTH.hs
-data RetryF m next where
-  Output    :: String -> next -> RetryF m next
-  Input     :: Read a => (a -> next) -> RetryF m next
-  WithRetry :: (RetryT m) m a -> (a -> next) -> RetryF m next
-  Retry     :: RetryF m next
-
--- data (Read a) => RetryF m a next =  Output String next
---                                   | Input (a -> next)
---                                   | WithRetry ((RetryT m) m a) (a -> next)
---                                   | Retry
-
--- type RetryT m = FreeT (RetryF m a)
-type RetryT m = FreeT (RetryF m)
-
-instance Functor (RetryF m) where
-  fmap f (Output s x) = Output s (f x)
-  fmap f (Input g) = Input (f . g)
-  fmap f (WithRetry block g) = WithRetry block (f . g)
-  fmap _ Retry = Retry
+type OpsM a = Free (OpsF a )
 
 -- magic
-makeFree ''Ops
+makeFree ''OpsF
+
+transact1 :: forall (m :: * -> *) a. MonadFree (OpsF a) m => OpsM a a -> m ()
+transact1 = undefined
 
 -- run foo in IO
 runFoo :: IO ()
 runFoo = putStrLn "someFunc"
 
-foo :: OpsM ()
+foo :: OpsM a b
 foo = do
   put "foo1" "value1"
   put "foo2" "value2"
   transact $ do
     (t1, t2) <- transact $ do
       v1 <- get "foo1"
-      v2 <- get "foo1"
+      v2 <- get "foo2"
       return (v1, v2)
     transact $ do
-      put "foo1" v2
-      put "foo2" v1
+      put "foo1" t2
+      put "foo2" t1
 
 
-runOps :: MonadIO m => OpsM a -> m a
+runOps :: MonadIO m => OpsM a a -> m a
 runOps = iterM run where
-  run :: MonadIO m => Ops (m a) -> m a
+  run :: MonadIO m => OpsF (m a) -> m a
   run = undefined
