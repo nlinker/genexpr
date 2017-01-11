09d1ae81f9b01976dd403607f0c2034deef9eae1
Nick Linker <nlinker@gmail.com>
2017-01-11 09:31:04 +0600

    Remove temp code

--- expr-gen/exe/Main.hs
+++ expr-gen/exe/Main.hs
@@ -5,10 +5,3 @@ import Control.Monad.Random
 
 main :: IO ()
 main = run
-
--- size :: Num a => [a] -> Int
-size :: Num a => [t] -> a
-size xs = loop xs 0
-  where
-    loop [] acc = acc
-    loop (_ : xs) acc = loop xs (acc + 1)
