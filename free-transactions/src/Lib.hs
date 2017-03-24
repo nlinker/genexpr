361346f3233d7c5c01857625ba7f8974a63fea90
Nick Linker <nlinker@gmail.com>
2017-03-24 17:13:55 -0500

    failed attempt with hasql-transaction

--- free-transactions/src/Lib.hs
+++ free-transactions/src/Lib.hs
@@ -35,9 +35,10 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 
 data Stuff = Stuff
-  { name :: Text
+  { id :: Int
+  , name :: Text
   , age :: Int
-  }
+  } deriving (Show, Eq)
 
 type Key = Text
 type Val = Text
@@ -120,37 +121,45 @@ defaultSettings = HC.settings dbHost dbPort dbUser dbPassword dbDatabase
     dbPassword = "postgres"
     dbDatabase = "notification_db"
 
-insertStuff :: Stuff -> HS.Session Int32
-insertStuff stuff = HS.query stuff stmt
+insertStuff :: HQ.Query Stuff Int32
+insertStuff = stmt
   where
     stmt = HQ.statement sqlText encoder decoder True
-    sqlText = "INSERT INTO stuff (name, age) VALUES ($1) RETURNING id;"
+    sqlText = "INSERT INTO stuffs (name, age) VALUES ($1, $2) RETURNING id;"
     encoder =
       contramap name (HE.value HE.text) <>
       contramap (fromIntegral . age) (HE.value HE.int4)
     decoder =
       HD.singleRow (HD.value HD.int4)
 
-readAllStuff :: HS.Session [Stuff]
-readAllStuff = HS.query () stmt
+readAllStuff :: HQ.Query () [Stuff]
+readAllStuff = stmt
   where
     stmt = HQ.statement sqlText encoder decoder True
-    sqlText = "SELECT id, name, age FROM stuff;"
+    sqlText = "SELECT id, name, age FROM stuffs;"
     encoder =
       HE.unit
     decoder =
-      HD.rowsList $ Stuff <$>
-        HD.value HD.text <*>
-        (fromIntegral <$> HD.value HD.int4)
+      HD.rowsList $ Stuff
+        <$> (fromIntegral <$> HD.value HD.int4)
+        <*> HD.value HD.text
+        <*> (fromIntegral <$> HD.value HD.int4)
 
+-- TODO WARNING: it seems impossible to integrate it with Free OpsF
+trans2 :: HT.Transaction [Stuff]
+trans2 = do
+  id1 <- HT.query (Stuff 0 "stuff1" 12) insertStuff
+--  liftIO $ print id1
+  id2 <- HT.query (Stuff 0 "stuff2" 23) insertStuff
+--  liftIO $ print id2
+  id3 <- HT.query (Stuff 0 "stuff3" 34) insertStuff
+--  liftIO $ print id3
+  HT.query () readAllStuff
 
-sqls :: [BS.ByteString]
-sqls =
-  [ "SELECT 1;"
-  , "SELECT 2;"
-  , "SELECT 3;"
-  , "SELECT 4;"
-  ]
+exampleTransaction2 :: HS.Session [Stuff]
+exampleTransaction2 = do
+  liftIO $ putStrLn "Hey"
+  HTS.transaction HT.ReadCommitted HT.Write trans2
 
 exampleTransaction :: HS.Session ()
 exampleTransaction = HTS.transaction HT.ReadCommitted HT.Write trans
@@ -159,17 +168,18 @@ exampleTransaction = HTS.transaction HT.ReadCommitted HT.Write trans
     prepareSqlExec sql = HQ.statement sql HE.unit HD.unit False
 
     queries :: [HQ.Query () ()]
-    queries = prepareSqlExec <$> sqls
+    queries = prepareSqlExec <$> ["SELECT 1", "SELECT 2"]
 
     trans :: HT.Transaction ()
     trans = void (sequence $ HT.query () <$> queries)
 
+-- do this here
 kick :: IO ()
 kick = do
   connE <- HC.acquire defaultSettings
   runE <- case connE of
     Left err -> error $ show err
-    Right conn -> HS.run exampleTransaction conn
+    Right conn -> HS.run exampleTransaction2 conn
   print runE
 
 -- global hash map
