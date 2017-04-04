f34192e593bf0e243e8c6c01a522793928262e36
Nick Linker <nlinker@gmail.com>
2017-04-04 02:00:08 -0500

    transactions with ExceptT work

--- free-transactions/src/TFM.hs
+++ free-transactions/src/TFM.hs
@@ -1,84 +1,37 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE FlexibleContexts #-}
+--{-# OPTIONS_GHC -Wall -fno-warn-unused-do-bind #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures             #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
 
 module TFM where
 
-import Debug.Trace
+import           Debug.Trace
 
-import Prelude hiding           (log)
-import Control.Monad            (when)
-import Control.Monad.IO.Class   (MonadIO, liftIO)
-import Control.Monad.Except     (ExceptT(..), MonadTrans, lift)
-import Control.Monad.Free
-import Control.Monad.Free.TH    (makeFree, makeFreeCon)
-import Control.Monad.Trans.Free (FreeT(..))
-import Data.Text                (Text, pack)
-import Data.HashMap             (Map, empty, insert, lookup)
-import Data.Function            (id)
-import Control.Monad.Except     (MonadError, runExceptT, catchError, throwError)
-import Control.Exception        (IOException(..), SomeException(..), Exception, throw)
-import Data.Typeable            (Typeable)
-import Text.Read                (readMaybe)
-import Data.Monoid              ((<>))
-import Control.Monad.Trans.Maybe (runMaybeT)
-import Data.Foldable            (msum)
+import           Control.Exception         (Exception, IOException (..), SomeException (..), throw)
+import           Control.Monad             (when)
+import           Control.Monad.Except      (ExceptT (..), MonadTrans, lift)
+import           Control.Monad.Except      (MonadError, catchError, runExceptT, throwError)
+import           Control.Monad.Free
+import           Control.Monad.Free.TH     (makeFree, makeFreeCon)
+import           Control.Monad.IO.Class    (MonadIO, liftIO)
+import           Control.Monad.Trans.Free  (FreeT (..))
+import           Control.Monad.Trans.Maybe (runMaybeT)
+import           Data.Foldable             (msum)
+import           Data.Function             (id)
+import           Data.HashMap              (Map, empty, insert, lookup)
+import           Data.Monoid               ((<>))
+import           Data.Text                 (Text, pack)
+import           Data.Typeable             (Typeable)
+import           Prelude                   hiding (log)
+import           Text.Read                 (readMaybe)
 
-type Key = Text
-type Val = Text
-
-data Err = Err deriving (Eq, Show)
-
---data TestF n where
---  Log      :: (MonadError Err m) => Text -> (m () -> n)         -> TestF n
---  Get      :: (MonadError Err m) => Key -> (m Val -> n)         -> TestF n
---  Put      :: (MonadError Err m) => Key -> Val -> (m () -> n)   -> TestF n
---  Rollback :: (MonadError Err m) => (m () -> n)                 -> TestF n
---  Commit   :: (MonadError Err m) => (m () -> n)                 -> TestF n
---  Transact :: (MonadError Err m) => TestFree m a -> (m a -> n)  -> TestF n
-
-data TestF n where
-  Log      :: (MonadError Err m) => Text -> (m () -> n)         -> TestF n
-  Get      :: (MonadError Err m) => Key -> (m Val -> n)         -> TestF n
-  Put      :: (MonadError Err m) => Key -> Val -> (m () -> n)   -> TestF n
-  Rollback :: (MonadError Err m) => (m () -> n)                 -> TestF n
-  Commit   :: (MonadError Err m) => (m () -> n)                 -> TestF n
---  Transact :: (MonadError Err m) => TestFree m a -> (m a -> n)  -> TestF n
-
-instance Functor TestF where
-  fmap f (Log msg n) = Log msg (f . n)
-  fmap f (Get k n) = Get k (f . n)
-  fmap f (Put k v n) = Put k v (f . n)
-  fmap f (Rollback n) = Rollback (f . n)
-  fmap f (Commit n) = Commit (f . n)
---  fmap f (Transact block n) = Transact block (f . n)
-
-type TestFree m a = FreeT TestF m a
---  deriving (Applicative, Functor, Monad, MonadFree (TestF tex))
-
-makeFree ''TestF
-
---log      :: (MonadFree TestF m) => Text -> m ()
---get      :: (MonadFree TestF m) => Key -> m Val
---put      :: (MonadFree TestF m) => Key -> Val -> m ()
---transact :: (MonadFree TestF m) => TestFree m a -> m a
---rollback :: (MonadFree TestF m) => m ()
---commit   :: (MonadFree TestF m) => m ()
-
--- either :: (a -> c) -> (b -> c) -> Either a b -> c
--- runExceptT :: ExceptT e m a -> m (Either e a)
-err :: (MonadError Err m) => Either Err a -> m a
-err = either throwError return
-
-a :: (MonadFree TestF m, MonadError Err m) => Text -> m ()
-a = undefined
-
-someFn :: ExceptT Err IO ()
-someFn = err (Left Err)
+data Err = Err Text deriving (Eq, Show)
 
 type TFM a = FreeT TransF (Either Err)
 
@@ -88,57 +41,58 @@ type TransFree = Free TransF
 data TransF next where
   Output    :: String ->                  (Either Err () -> next) -> TransF next
   Input     :: Read a =>                  (Either Err a -> next)  -> TransF next
-  Transact  :: Show a => ExceptT Err TransFree a -> (Either Err a -> next)  -> TransF next
-  Reset     ::                            (Either Err () -> next) -> TransF next
+  Transact  :: Show a => TransFree (Either Err a) -> (Either Err a -> next)  -> TransF next
+  Rollback  ::                            (Either Err () -> next) -> TransF next
 
 instance Functor TransF where
   fmap f (Output s n) = Output s (f . n)
   fmap f (Input n) = Input (f . n)
   fmap f (Transact block n) = Transact block (f . n)
-  fmap f (Reset n) = Reset (f . n)
-
+  fmap f (Rollback n) = Rollback (f . n)
 
 makeFree ''TransF
 
 -- runExceptT :: ExceptT e m a -> m (Either e a)
 
-output1 :: MonadFree TransF m => String -> ExceptT Err m ()
+output1 :: String -> ExceptT Err TransFree ()
 output1 s = ExceptT $ liftF $ Output s id
 
-input1 :: (MonadFree TransF m, Read a) => ExceptT Err m a
+input1 :: (Read a) => ExceptT Err TransFree a
 input1 = ExceptT $ liftF $ Input id
 
--- liftF :: (Functor f, MonadFree f m) => f a -> m a
--- liftF :: TransF (Either Err a) -> m (Either Err a)
--- Transact  :: Show a => ExceptT Err TransFree a -> (ExceptT Err TransFree a -> next) -> TransF next
--- transact1 :: MonadFree TransF m => TransFree ae -> m ae
-transact1 :: (MonadFree TransF m, Show a) => ExceptT Err TransFree a -> m (Either Err a)
-transact1 block = liftF $ Transact block id
+--transact :: Show a => TransFree (Either Err a) -> TransFree (Either Err a)
 
-unwrap :: ExceptT Err TransFree a -> a
-unwrap exc = do
-  let eth = runExceptT exc
-  undefined
---f :: TransF (ExceptT Err TransFree a) -> m (Either Err a)
---f = liftF _
+transact1 :: Show a => ExceptT Err TransFree a -> ExceptT Err TransFree a
+transact1 = ExceptT . inner . runExceptT
+  where
+    inner :: Show a => TransFree (Either Err a) -> TransFree (Either Err a)
+    inner block = liftF $ Transact block id
 
-reset1 :: MonadFree TransF m => ExceptT Err m ()
-reset1 = ExceptT $ liftF $ Reset id
+rollback1 :: ExceptT Err TransFree ()
+rollback1 = ExceptT $ liftF $ Rollback id
 
 main :: IO ()
 main = do
   r <- fireIO test
   print r
 
-test :: TransFree (Either Err ())
+test0 :: TransFree (Either Err ())
+test0 = do
+  eth <- transact rollback
+  return eth
+
+test :: TransFree (Either Err Integer)
 test = runExceptT $ do
-  n <- transact1 $ do
-    n <- input1
-    when (n <= 0) $ do
-      output1 "The number should be positive."
-      reset1
-    return $ Right n
-  output1 $ "You've just entered " ++ show (n :: Either Err (Either Err Integer))
+  (k :: Integer) <- input1
+  output1 $ "k = " <> show k
+  (n :: Integer) <- transact1 $ do
+    n' <- input1
+    when (n' >= 0) $ do
+      output1 "The number should be negative."
+      rollback1
+    return n'
+  output1 $ "You've just entered " ++ show n
+  return n
 
 fireIO :: MonadIO m => TransFree a -> m a
 fireIO = iterM runIO
@@ -155,21 +109,40 @@ runIO (Input next) = do
     readE :: (Read a1) => String -> Either Err a1
     readE s = case readMaybe s of
       Just x -> Right x
-      Nothing -> Left Err
+      Nothing -> Left $ Err $ "cannot read " <> pack s
 runIO (Transact block next) = do
   -- eth :: Either Err a
+  -- fireIO block :: m (Either Err a)
   -- fireIO :: MonadIO m => TransFree a -> m a
-  -- block :: ExceptT Err TransFree a
+  -- block :: TransFree (Either Err a)
   -- runExceptT :: ExceptT e m a -> m (Either e a)
-  -- runExceptT block :: Free TransF (Either Err a)
-  -- runExceptT block :: TransFree (Either Err a)
-  -- fireIO (runExceptT block) :: MonadIO m => m (Either Err a)
-  eth <- fireIO $ runExceptT block
-  traceShowM $ "----> " <> show eth
+  eth <- fireIO block
+  traceM $ "----> " <> show eth
   case eth of
     Left err -> next $ Left err
     Right a -> next $ Right a
-runIO (Reset next) = next $ Left Err
+runIO (Rollback next) = next $ Left $ Err "reset"
+
+
+-- TODO how to make free monads with monad constraints
+--type TestFree m a = FreeT TestF m a
+
+--data TestF n where
+--  Log      :: (MonadError Err m) => Text -> (m () -> n)         -> TestF n
+--  Get      :: (MonadError Err m) => Key -> (m Val -> n)         -> TestF n
+--  Put      :: (MonadError Err m) => Key -> Val -> (m () -> n)   -> TestF n
+--  Rollback :: (MonadError Err m) => (m () -> n)                 -> TestF n
+--  Commit   :: (MonadError Err m) => (m () -> n)                 -> TestF n
+--  Transact :: (MonadError Err m) => TestFree m a -> (m a -> n)  -> TestF n
+
+--instance Functor TestF where
+--  fmap f (Log msg n) = Log msg (f . n)
+--  fmap f (Get k n) = Get k (f . n)
+--  fmap f (Put k v n) = Put k v (f . n)
+--  fmap f (Rollback n) = Rollback (f . n)
+--  fmap f (Commit n) = Commit (f . n)
+--  fmap f (Transact block n) = Transact block (f . n)
+--makeFree ''TestF
 
 --foo :: (MonadFree TestF m, MonadError Err m) => m ()
 --foo = do
