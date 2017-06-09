f673a1da8bf07d82e449ce58c069d5bd7ef21da9
Nick Linker <nlinker@gmail.com>
2017-06-10 02:34:15 +0600

    introduce Point type

--- sokoban/src/Sokoban.hs
+++ sokoban/src/Sokoban.hs
@@ -1,10 +1,17 @@
 module Sokoban where
 
+import Data.Vector (Vector)
+
 mainS :: IO ()
 mainS = undefined
 
 data Direction = U | D | L | R
   deriving (Eq, Show)
+
+data Point = Point
+  { x :: Integer
+  , y :: Integer
+  } deriving (Eq, Show)
 
 data Cell =
     Char Direction
@@ -16,9 +23,13 @@ data Cell =
   | CharOverHole Direction
   deriving (Eq, Show)
 
-delta :: Direction -> (Integer, Integer)
-delta U = (0, -1)
-delta D = (0, 1)
-delta L = (-1, 0)
-delta R = (1, -1)
+data Level = Level
+ { cells        :: Vector (Vector Cell)
+ , charPosition :: Point
+ } deriving (Eq, Show)
 
+delta :: Direction -> Point
+delta U = Point   0  (-1)
+delta D = Point   0    1
+delta L = Point (-1)   0
+delta R = Point   1    0
