97bc3502e7594f6eeca2d374d8874a82eeab8808
Nick Linker <nlinker@gmail.com>
2017-06-11 02:05:18 +0600

    add some levels and level parser

--- sokoban/src/Sokoban.hs
+++ sokoban/src/Sokoban.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+
 module Sokoban where
 
+import qualified Data.ByteString       as B
+import qualified Data.ByteString.Char8 as C
+import Helper (str)
 import Data.Vector (Vector)
 
 mainS :: IO ()
@@ -14,13 +21,13 @@ data Point = Point
   } deriving (Eq, Show)
 
 data Cell =
-    Char Direction
+    Worker Direction
   | Hole
-  | Stone
+  | Box
   | Wall
   | Empty
-  | StoneOverHole
-  | CharOverHole Direction
+  | BoxOnHole
+  | WorkerOnHole Direction
   deriving (Eq, Show)
 
 data Level = Level
@@ -33,3 +40,35 @@ delta U = Point   0  (-1)
 delta D = Point   0    1
 delta L = Point (-1)   0
 delta R = Point   1    0
+
+parseCell :: Char -> Maybe Cell
+parseCell '@' = Just (Worker U)
+parseCell '+' = Just (WorkerOnHole U)
+parseCell ' ' = Just Empty
+parseCell '#' = Just Wall
+parseCell '.' = Just Hole
+parseCell '$' = Just Box
+parseCell '*' = Just BoxOnHole
+parseCell _   = Nothing
+
+parseLevel :: B.ByteString -> Maybe Level
+parseLevel s = do
+  let lns = C.lines s
+  let chars = map C.unpack lns
+  let ijs = [[(i, j) | i <- [0 ..]] | j <- [0 ..]]
+  let elems :: [((Integer, Integer), Char)]
+      elems = concat $ zipWith zip ijs chars
+  Nothing
+
+
+level :: B.ByteString
+level = [str|
+    #####
+   ## . #
+   # . $#
+  ##$#..#
+  #@$ * #
+  # $   #
+  ###   #
+    #####
+|]
