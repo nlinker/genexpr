bc43fbb70787fe5940cc42023bd9c74eba2600e5
Nick Linker <nlinker@gmail.com>
2017-07-20 00:45:40 +0600

    added finger trees test

--- sokoban/src/Sokoban.hs
+++ sokoban/src/Sokoban.hs
@@ -1,19 +1,30 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE QuasiQuotes           #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
 module Sokoban where
 
+import Control.Arrow (second)
+import Control.Monad (foldM)
+import Data.Monoid   (Sum(..))
+import Data.Vector   (Vector)
+import Helper        (str)
+
 import qualified Data.ByteString       as B
 import qualified Data.ByteString.Char8 as C
-import Control.Arrow (second)
-import Helper (str)
-import Data.Vector (Vector)
+import qualified Data.FingerTree       as FT
+import qualified Data.RBTree           as RB
 
 mainS :: IO ()
 mainS = undefined
 
-data Direction = U | D | L | R
+data Direction
+  = U
+  | D
+  | L
+  | R
   deriving (Eq, Show)
 
 data Point = Point
@@ -21,8 +32,8 @@ data Point = Point
   , y :: Integer
   } deriving (Eq, Show)
 
-data Cell =
-    Worker Direction
+data Cell
+  = Worker Direction
   | Hole
   | Box
   | Wall
@@ -32,15 +43,15 @@ data Cell =
   deriving (Eq, Show)
 
 data Level = Level
- { cells        :: Vector (Vector Cell)
- , charPosition :: Point
- } deriving (Eq, Show)
+  { cells        :: Vector (Vector Cell)
+  , charPosition :: Point
+  } deriving (Eq, Show)
 
 delta :: Direction -> Point
-delta U = Point   0  (-1)
-delta D = Point   0    1
-delta L = Point (-1)   0
-delta R = Point   1    0
+delta U = Point 0 (-1)
+delta D = Point 0 1
+delta L = Point (-1) 0
+delta R = Point 1 0
 
 parseCell :: Char -> Maybe Cell
 parseCell '@' = Just (Worker U)
@@ -53,8 +64,9 @@ parseCell '*' = Just BoxOnHole
 parseCell _   = Nothing
 
 parseLevel :: B.ByteString -> Maybe Level
-parseLevel str = do
+parseLevel str
   -- break string into lines
+ = do
   let chars = map C.unpack $ C.lines str
   let ijs = [[(i, j) | i <- [0 ..]] | j <- [0 ..]]
   let elems = concat $ zipWith zip ijs chars
@@ -63,7 +75,8 @@ parseLevel str = do
   Nothing
 
 level :: B.ByteString
-level = [str|
+level =
+  [str|
     #####
    ## . #
    # . $#
@@ -73,3 +86,33 @@ level = [str|
   ###   #
     #####
 |]
+
+newtype Elem a =
+  Elem a
+  deriving (Eq)
+
+instance Show a => Show (Elem a) where
+  show (Elem x) = show x
+
+instance FT.Measured (Sum Int) (Elem a) where
+  measure (Elem _) = Sum 1
+
+type MyFTree = FT.FingerTree (Sum Int) (Elem Int)
+
+testRB :: IO ()
+testRB = do
+  let xs = [1 .. 7] :: [Integer]
+  ts <- foldM doStuff RB.E xs :: IO (RB.Tree Integer)
+  putStrLn $ ">>> final = " ++ show ts
+  where
+    doStuff t x = do
+      let t' = RB.insert x t
+      putStrLn $ ">>> insert x t = " ++ show t'
+      return t'
+
+testFT :: IO MyFTree
+testFT = do
+  let xs = map Elem [1 .. 7] :: [Elem Int]
+  let ft = FT.fromList xs
+  putStrLn $ ">>> ft=" ++ show ft
+  return ft
