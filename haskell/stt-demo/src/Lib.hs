22c0e1792a592457a83277f29665ee2a3de1adf8
Nick Linker <nlinker@gmail.com>
2019-10-24 02:15:46 +0600

    stt-demo: build the absent instance PrimMonad for STT

--- haskell/stt-demo/src/Lib.hs
+++ haskell/stt-demo/src/Lib.hs
@@ -4,53 +4,63 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TupleSections         #-}
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE Strict #-}
+{-# LANGUAGE Strict                #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UnboxedTuples         #-}
 
 module Lib where
 
-import Data.Hashable                   (Hashable)
+import Control.Monad                   (filterM, forM_, when)
+import Control.Monad.Primitive         (PrimMonad, PrimState, primitive)
 import Control.Monad.ST                (ST, runST)
-import Control.Monad.ST.Trans          (STT, runSTT)
-import Control.Monad.ST.Trans.Internal (liftST)
-import Data.Maybe                      (isJust, fromMaybe)
+import Control.Monad.ST.Trans          (runSTT)
+import Control.Monad.ST.Trans.Internal (STT(..), STTRet(..), liftST)
+import Data.Hashable                   (Hashable)
+import Data.Maybe                      (fromMaybe, isJust)
 import GHC.Generics                    (Generic)
-import Control.Monad                   (forM_, when, filterM)
-import Control.Monad.Primitive         (PrimMonad, PrimState)
 
-import qualified Data.Heap.Mutable.ModelD   as HPM
 import qualified Data.HashMap.Mutable.Basic as HM
-import Debug.Trace
+import qualified Data.Heap.Mutable.ModelD   as HPM
+import           Debug.Trace
 
 newtype Min =
   Min Int
   deriving (Show, Read, Eq, Ord)
 
-data Point = Point Int Int deriving (Eq, Show, Generic, Hashable)
+data Point =
+  Point Int Int
+  deriving (Eq, Show, Generic, Hashable)
+
 instance Ord Point where
   compare (Point i1 j1) (Point i2 j2) = compare (i1, i2) (j1, j2)
 
-someAlgorithm :: forall m p . (Monad m, Hashable p, Ord p, Show p) => p -> p -> (p -> m Int) -> m [p]
+instance (Monad m) => PrimMonad (STT s m) where
+  type PrimState (STT s m) = s
+  primitive f =
+    STT $ \s ->
+      case f s of
+        (# t, a #) -> return (STTRet t a)
+  {-# INLINE primitive #-}
+
+someAlgorithm ::
+     forall m p. (Monad m, Hashable p, Ord p, Show p)
+  => p
+  -> p
+  -> (p -> m Int)
+  -> m [p]
 someAlgorithm src dst rang = do
-  path <- runSTT $ do
-    table <- HM.new
-    HM.insert table src dst
-    HM.insert table dst src
-    has <- member table src
-    traceShowM $ "has = " <> show has
-    return [] 
+  path <-
+    runSTT $ do
+      table <- HM.new
+      HM.insert table src dst
+      HM.insert table dst src
+      has <- member table src
+      traceShowM $ "has = " <> show has
+      return []
   traceShowM path
   return path
   where
-    -- member :: (Monad m, Hashable k, Eq k) => HM.MHashMap s k a -> k -> STT s m Bool
     member table p = do
       value <- HM.lookup table p
-      return $ isJust value 
+      return $ isJust value
