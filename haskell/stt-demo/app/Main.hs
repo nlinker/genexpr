305499f8d937d8a055bd627e0fe17b82fdb26d06
Nick Linker <nlinker@gmail.com>
2019-10-26 15:37:16 +0600

    stt-demo: implement Collatz walk

--- haskell/stt-demo/app/Main.hs
+++ haskell/stt-demo/app/Main.hs
@@ -1,30 +1,6 @@
 module Main where
 
-import Lib
-import Control.Monad (filterM)
+import Lib as L
 
 main :: IO ()
-main = do
-  let src = Point 1 1
-  let dst = Point 2 2
-  let rang p = return
-  solver <- buildMoveSolver [Point 1 2, Point 2 1]  
-  path <- aStarFind solver src dst (return . (== dst))
-  putStrLn $ "path = " <> show path
-
-buildMoveSolver :: [Point] -> IO (AStarSolver IO Point)
-buildMoveSolver walls = do
-  let n = 5 
-  let p2i (Point i j) = i * n + j
-  let i2p k = Point (k `div` n) (k `mod` n)
-  return $ AStarSolver {neighbors = neighbors, distance = distance, heuristic = heuristic, p2i = p2i, i2p = i2p}
-  where
-    neighbors p0 = do
-      let isAccessible p =
-            if p `elem` walls
-              then return False
-              else return True
-      let neighs = map (movePoint p0) [U, D, L, R]
-      filterM isAccessible neighs
-    distance np p0 = return $ fromEnum (np /= p0)
-    heuristic (Point i1 j1) (Point i2 j2) = return $ abs (i1 - i2) + abs (j1 - j2)
+main = L.testAlg
--- haskell/stt-demo/src/Lib.hs
+++ haskell/stt-demo/src/Lib.hs
@@ -17,7 +17,7 @@ import Control.Monad.Primitive         (PrimMonad, PrimState, primitive)
 import Control.Monad.ST                (ST, runST)
 import Control.Monad.ST.Trans          (runSTT)
 import Control.Monad.ST.Trans.Internal (STT(..), STTRet(..), liftST)
-import Control.Monad.State             (lift)
+import Control.Monad.State             (lift, MonadState, get, put, modify, evalStateT)
 import Data.Hashable                   (Hashable)
 import Data.Maybe                      (fromMaybe, isJust)
 import GHC.Generics                    (Generic)
@@ -29,42 +29,6 @@ import qualified Data.HashMap.Mutable.Basic as HM
 import qualified Data.Heap.Mutable.ModelD   as HPM
 import           Debug.Trace
 
-data Direction
-  = U
-  | D
-  | L
-  | R
-  deriving (Eq, Ord, Show, Generic, Hashable)
-
-data Point =
-  Point Int Int
-  deriving (Eq, Show, Generic, Hashable)
-
-instance Ord Point where
-  compare (Point i1 j1) (Point i2 j2) = compare (i1, i2) (j1, j2)
-
-newtype Min =
-  Min Int
-  deriving (Show, Read, Eq, Ord)
-
-instance Semigroup Min where
-  (<>) (Min a) (Min b) = Min (min a b)
-
-instance Monoid Min where
-  mempty = Min maxBound
-
-newtype MyElement = MyElement { getMyElement :: Int }
-  deriving (Show,Read,Eq,Ord)
-
-data AStarSolver m p where
-  AStarSolver :: (Monad m, Hashable p, Ord p) =>
-    { neighbors :: p -> m [p]
-    , distance  :: p -> p -> m Int -- for adjacent points only
-    , heuristic :: p -> p -> m Int
-    , p2i       :: p -> Int
-    , i2p       :: Int -> p
-    } -> AStarSolver m p
-
 instance (Monad m) => PrimMonad (STT s m) where
   type PrimState (STT s m) = s
   primitive f =
@@ -73,103 +37,64 @@ instance (Monad m) => PrimMonad (STT s m) where
         (# t, a #) -> return (STTRet t a)
   {-# INLINE primitive #-}
 
-aStarFind :: forall m p . (Monad m, Hashable p, Ord p, Show p) => AStarSolver m p -> p -> p -> (p -> m Bool) -> m [p]
-aStarFind solver src dst stopCond = do
-  let maxNodesCount = 1000000
-  path <- runSTT $ do
-    heap <- HPM.new maxNodesCount           :: STT s m (HPM.Heap s Min)
-    openList <- HM.new                      :: STT s m (HM.MHashMap s p (p, Int))
-    closedList <- HM.newSized maxNodesCount :: STT s m (HM.MHashMap s p p)
-    HM.insert openList src (src, 0)
-    HPM.unsafePush (mempty :: Min) (p2i solver src) heap
-    let aStarFindRec = do
-          top' <- HPM.pop heap -- remove the minimum and return
-          case top' of
-              Nothing -> return []
-              Just (_fscore, i) -> do
-                let p0 = i2p solver i
-                finished <- lift $ stopCond p0
-                if finished
-                  then do
-                    -- HM.insert p0 (weight0 ^. parent) closedList
-                    pg' <- HM.lookup openList p0
-                    let (parent, _gscore) = fromMaybe (error "[qm|{p0} is not found in openList|]") pg'
-                    HM.insert closedList p0 parent
-                    backtraceST closedList p0
-                  else do
-                    pg' <- HM.lookup openList p0
-                    let (parent, gscore) = fromMaybe (error "[qm|{p0} is not found in openList|]") pg'
-                    HM.insert closedList p0 parent
-                    neighbors <- lift $ neighbors solver p0
-                    neighPoints <- filterM (\p -> not <$> member closedList p) neighbors
-                    forM_ neighPoints $ \np -> do
-                      dist <- lift $ distance solver np p0
-                      hue <- lift $ heuristic solver np dst
-                      let gscoreNp = gscore + dist
-                      let fscoreNp = Min (gscore + dist + hue)
-                      pg' <- HM.lookup openList np
-                      case pg' of
-                        Just (parent, gscore) | gscoreNp < gscore -> do
-                          -- the neighbour can be reached with smaller cost - change priority
-                          -- otherwise don't touch the neighbour, it will be taken by open_list.pop()
-                          -- openList .= Q.insert np f1 w1 openList0
-                          HPM.push fscoreNp (p2i solver np) heap
-                          HM.insert openList np (parent, gscoreNp)
-                        Nothing -> do
-                          -- the neighbour is new
-                          -- openList .= Q.insert np f1 w1 openList0
-                          HPM.push fscoreNp (p2i solver np) heap
-                          HM.insert openList np (parent, gscoreNp)
-                        _ -> return ()
-                    aStarFindRec
-    aStarFindRec
-  return path
+data Direction
+  = U
+  | D
+  | L
+  | R
+  deriving (Eq, Ord, Enum, Show)
+
+data Point =
+  Point Int Int
+  deriving (Eq, Generic, Hashable)
+
+instance Show Point where
+  show (Point i j) = "(" <> show i <> ", " <> show j <> ")"
+
+data Alg m p where
+  Alg :: (Monad m, Eq p) => {
+    select :: m Direction,
+    step :: Direction -> p -> m p,
+    stopCond :: p -> m Bool
+  } -> Alg m p
+
+buildAlg :: forall m . MonadState Integer m => Point -> m (Alg m Point)
+buildAlg dst = return $ Alg { select = select, step = step, stopCond = stopCond }
   where
-    -- member :: forall m p . (PrimMonad m, Hashable p, Ord p, Show p) => p -> m Bool
-    member :: (Monad m, Hashable k, Eq k) => HM.MHashMap s k a -> k -> STT s m Bool
-    member hm p = do
-      v' <- HM.lookup hm p
-      return $ isJust v'
+    select :: m Direction
+    select = do
+      modify $ \n -> if even n then n `div` 2 else n * 3 + 1
+      state <- get
+      return $ toEnum $ fromIntegral state `mod` 4
+    
+    step :: Direction -> Point -> m Point
+    step d (Point i j) = return $
+      case d of
+        U -> Point (i - 1) (j)
+        D -> Point (i + 1) (j)
+        L -> Point (i) (j - 1)
+        R -> Point (i) (j + 1)
+    
+    stopCond :: Point -> m Bool 
+    stopCond p = return $ dst == p
 
-backtraceST :: forall m p . (PrimMonad m, Eq p, Hashable p) => HM.MHashMap (PrimState m) p p -> p -> m [p]
-backtraceST closedList dst = backtraceRec dst [dst]
+algorithm :: forall m p . (Monad m, Show p) => Alg m p -> p -> m [p]
+algorithm alg start = go start []
   where
-    -- we repeatedly lookup for the parent of the current node
-    backtraceRec :: (PrimMonad m, Eq p) => p -> [p] -> m [p]
-    backtraceRec current acc = do
-      parent' <- HM.lookup closedList current
-      case parent' of
-        Nothing -> return []
-        Just parent
-          | current == parent -> return acc
-        Just parent -> backtraceRec parent (parent : acc)
+    go :: p -> [p] -> m [p]
+    go p1 xs = do
+      stop <- stopCond alg p1
+      if stop then return xs
+      else do
+        dir <- select alg
+        p2 <- step alg dir p1
+        traceM $ show p2
+        go p2 (p2:xs)
 
-movePoint :: Point -> Direction -> Point
-movePoint (Point i j) d =
-  case d of
-    U -> Point (i - 1) (j)
-    D -> Point (i + 1) (j)
-    L -> Point (i) (j - 1)
-    R -> Point (i) (j + 1)
-
--- λ> :rr heapMatchesList $ (\(f,s) -> (Min f, MyElement s)) <$> [(1,4),(2,3),(3,2),(4,1)]
-heapMatchesList :: [(Min,MyElement)] -> Bool
-heapMatchesList xs' = runIdentity $ do
-  let xs = coerce xs' :: [(Min,Int)]
-  let xsSet = fmap (\(p,e) -> (e,p)) xs
-  let heapRes = runST $ do
-        h <- HPM.new 1000
-        HPM.pushList xs h
-        ij0 <- HPM.pop h
-        traceM $ "ij0=" <> show ij0
-        HPM.push (Min 2) 4 h
-        ij1 <- HPM.pop h
-        traceM $ "ij1=" <> show ij1
-        ij2 <- HPM.pop h
-        traceM $ "ij2=" <> show ij2
-        ij3 <- HPM.pop h
-        traceM $ "ij3=" <> show ij3
-        ij4 <- HPM.pop h
-        traceM $ "ij4=" <> show ij4
-        HPM.popAll h
-  return $ trace [qm| xs={xs}\nheapRes={heapRes}|] $ True
+testAlg :: IO ()
+testAlg = do
+  let state = 13
+  path <- flip evalStateT state $ do
+        alg <- buildAlg (Point 0 0)
+        algorithm alg (Point 3 2)
+  putStrLn $ "path = " <> show path
